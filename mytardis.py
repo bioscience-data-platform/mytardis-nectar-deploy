1a6d18af034dc3de628c9dfe241db195ea374010
Iman Yusuf <iman.yusuf@rmit.edu.au>
2012-09-28 10:53:04 +1000

    Functions for creating and destroying a VM instance are completed

--- mytardis.py
+++ mytardis.py
@@ -6,9 +6,9 @@ import sys
 import time
 
 import logging
-import boto
 
 from nectarconnector import *
+from chefclient import *
 
 def start():
 
@@ -76,176 +76,18 @@ def start():
         connection = create_cloud_connection(settings)
         create_VM_instance(settings, connection)
         
-        print step
+        
     elif step == "test":
         print step, ip_address
     elif step == "destroy":
         connection = create_cloud_connection(settings)
         destroy_VM_instance(settings, connection, ip_address)
-        print step, ip_address
+    
     
     else:
         print "Not create"
         
-    '''
-        connection.run_instances(image_id="ami-0000000d",
-                                 min_count=number_vm,
-                                 max_count=number_vm,
-                                 key_name="nana-sync", 
-                                 security_groups=["ssh","http","icmp"], 
-                                 instance_type='m1.small')
-        while 1:
-            reservations = connection.get_all_instances()
-            instances = reservations[0].instances
-            instance = instances[0]
-            if instance:   
-                 state = instance.state
-                 print state, instance.ip_address
-                 if state == "running":
-                     break
-                 else:
-                     time.sleep(1)
-        time.sleep(10)
-    '''
-    
         
-        
-    """      
-    # get command line options
-    parser = OptionParser()
-    parser.add_option("-n", "--nodeid", dest="instance_id",
-                      help="The instance id from the cloud infrastructure")
-    parser.add_option("-i", "--inputdir", dest="input_dir",
-                      help="The local directory holding \
-                      input files for the task")
-    parser.add_option("-o", "--outputdir", dest="output_dir",
-                      help="The local directory which will \
-                      hold output files for the task")
-    parser.add_option("-g", "--group", dest="group_id",
-                      help="The group id from the cloud infrastructure")
-    parser.add_option("-v", "--number-vm-instances", type="int",
-                      dest="number_vm_instances",
-                      help="The number of VM instances to " +
-                      "be created as a group")
-    parser.add_option("-s", "--seed", dest="seed",
-                      help="The master seed that generates all other seeds")
-
-    (options, args) = parser.parse_args()
-
-    if 'create' in args:
-        if options.number_vm_instances:
-            res = create_environ(options.number_vm_instances, settings)
-            logger.debug(res)
-        else:
-            logging.error("enter number of VM instances to be created")
-            parser.print_help()
-            sys.exit(1)
-
-    elif 'setup' in args:
-        if options.group_id:
-            group_id = options.group_id
-            setup_multi_task(group_id, settings)
-        else:
-            logging.error("enter nodeid of the package")
-            parser.print_help()
-            sys.exit(1)
-
-    elif 'run' in args:
-        if options.group_id:
-
-            group_id = options.group_id
-
-            if not options.output_dir:
-                logging.error("specify output directory")
-                parser.print_help()
-                sys.exit(1)
-            elif os.path.isdir(options.output_dir):
-                logging.error("output directory already exists")
-                sys.exit(1)
-            else:
-                try:
-                    os.mkdir(options.output_dir)
-                except OSError:
-                    logger.error("output directory %s already exists" % options.output_dir)
-                    sys.exit(1)
-
-            prepare_multi_input(group_id, options.input_dir,
-                                settings, options.seed)
-
-            try:
-                pids = run_multi_task(group_id, options.input_dir, settings)
-            except PackageFailedError, e:
-                logger.error(e)
-                logger.error("unable to start packages")
-                #TODO: cleanup node of copied input files etc.
-                sys.exit(1)
-
-            while (not packages_complete(group_id,
-                                         options.output_dir,
-                                         settings)):
-                print("job is running.  Wait or CTRL-C to exit here. \
-                 run 'check' command to poll again")
-                time.sleep(settings.SLEEP_TIME)
-
-        else:
-            logging.error("enter group id of the run")
-            parser.print_help()
-            sys.exit(1)
-
-    elif 'check' in args:
-        if options.group_id:
-            if not options.output_dir:
-                logging.error("specify output directory")
-                parser.print_help()
-                sys.exit(1)
-
-            group_id = options.group_id
-            is_finished = packages_complete(group_id,
-                                            options.output_dir,
-                                            settings)
-
-            if is_finished:
-                print "done. output is available at %s" % options.output_dir
-            else:
-                print "job still running"
-        else:
-            logger.error("enter group id of the run")
-            parser.print_help()
-            sys.exit(1)
-
-    elif 'teardown' in args or 'teardown_all' in args:
-        # TODO: make sure that the instance we are tearing down is the one
-        # that is running the package and no some random VM, probably by
-        # logging in and checking state.
-        if options.group_id:
-            all_instances = collect_instances(settings, group_id=options.group_id)
-            if confirm_teardown(settings, all_instances):
-                destroy_environ(settings, all_instances)
-        elif options.instance_id:
-            all_instances = collect_instances(settings, instance_id=options.instance_id)
-            if confirm_teardown(settings, all_instances):
-                destroy_environ(settings, all_instances)
-        elif 'teardown_all' in args:
-            all_instances = collect_instances(settings, all_VM=True)
-            if confirm_teardown(settings, all_instances):
-                destroy_environ(settings, all_instances)
-        else:
-            logger.error("Enter either group id or instance id of the package")
-            parser.print_help()
-            sys.exit(1)
-
-#    elif 'print' in args:
-#        print_running_node_id(settings)
-
-    elif 'info' in args:
-        print "Summary of Computing Environment"
-        all_instances = collect_instances(settings, all_VM=True)
-        print_all_information(settings, all_instances)
-
-    else:
-        parser.print_help()
-
-   """
 if __name__ == '__main__':
     begins = time.time()
     start()
--- nectarconnector.py
+++ nectarconnector.py
@@ -1,73 +1,137 @@
 #import paramiko
+import sys
 import time
+import traceback
 
-import boto
-from boto.ec2.connection import EC2Connection
-from boto.ec2.regioninfo import *
+from libcloud.compute.types import Provider
+from libcloud.compute.types import NodeState
+from libcloud.compute.providers import get_driver
 
+NODE_STATE = ['RUNNING', 'REBOOTING', 'TERMINATED', 'PENDING', 'UNKNOWN']
 
 def create_cloud_connection(settings):
-    region = RegionInfo(name="NeCTAR", endpoint="nova.rc.nectar.org.au")
-    #logger.debug("Connecting to region... %s" % region)
-    print("Connecting to region... %s" % region)
-    connection = boto.connect_ec2(
-                                  aws_access_key_id=settings.EC2_ACCESS_KEY,
-                                  aws_secret_access_key=settings.EC2_SECRET_KEY,
-                                  is_secure=False,
-                                  region=region,
-                                  port=8773,
-                                  path="/services/Cloud")
+    OpenstackDriver = get_driver(Provider.EUCALYPTUS)
+    print("Connecting... %s" % OpenstackDriver)
+    connection = OpenstackDriver(settings.EC2_ACCESS_KEY, secret=settings.EC2_SECRET_KEY,
+                           host="nova.rc.nectar.org.au", secure=False,
+                           port=8773, path="/services/Cloud")
     #logger.debug("Connected")
     print("Connected")
     return connection
 
 
 def create_VM_instance(settings, connection):
-    connection.run_instances(image_id=settings.VM_IMAGE,
-                             key_name=settings.PRIVATE_KEY_NAME, 
-                             security_groups=settings.SECURITY_GROUP, 
-                             instance_type=settings.VM_SIZE)
-    while True:
-        reservations = connection.get_all_instances()
-        instance = reservations[0].instances[0]
-        if instance:   
-             state = instance.state
-             print state, instance.ip_address
-             if state == "running":
-                 break
-             else:
-                 time.sleep(settings.CLOUD_SLEEP_INTERVAL)
-        #check until successfull connection can be made
-                 
+    """
+        Create the Nectar VM instance and return ip_address
+    """
+    images = connection.list_images()
+    sizes = connection.list_sizes()
+
+    image1 = [i for i in images if i.id == settings.VM_IMAGE][0]
+    size1 = [i for i in sizes if i.id == settings.VM_SIZE][0]
+    
+    new_instance = None
+    try:
+        print("Creating VM instance")
+        new_instance = connection.create_node(name="New Centos VM instance",
+                                              size=size1, 
+                                              image=image1, 
+                                              ex_keyname=settings.PRIVATE_KEY_NAME,
+                                              ex_securitygroup=settings.SECURITY_GROUP)
+    except Exception, e:
+        if "QuotaError" in e[0]:
+            print " Quota Limit Reached: "
+        else:
+            traceback.print_exc(file=sys.stdout)
+       
+    if new_instance:
+        ip_address = _wait_for_instance_to_start_running(settings, connection, new_instance)
+        print 'Created VM instance with IP: %s' % ip_address
+        return ip_address
+              
 def destroy_VM_instance(settings, connection, ip_address):
-    instance = _get_this_instance(connection, ip_address)
-    if not instance:
-        print "VM instance %s Unknown " % ip_address
+    """
+        Terminate
+            - all instances, or
+            - a group of instances, or
+            - a single instance
+    """
+    print("Terminating VM instance %s" % ip_address)
+    if _is_instance_running(connection, ip_address):
+        try:
+            instance = _get_this_instance(connection, ip_address, ip_given=True)
+            if not confirm_teardown(settings):
+                return
+            connection.destroy_node(instance)
+            _wait_for_instance_to_terminate(settings, connection, ip_address)
+        except Exception:
+            traceback.print_exc(file=sys.stdout)
     else:
-        instance_list=[]
-        instance_list.append(instance.id)
-        connection.terminate_instances(instance_list)
-        _wait_for_instance_to_terminate(connection, ip_address)
-        
-        
+        print "VM instance with IP %s doesn't exist" % ip_address
 
 
-def _get_this_instance(connection, ip_address):
-    reservations = connection.get_all_instances()
-    for reservation in reservations:
-        instances = reservation.instances
-        for instance in instances:
-            if ip_address == instance.ip_address:
-                print instance
+def _get_this_instance(connection, instance_id_ip, ip_given=False):
+    instances = connection.list_nodes()
+    for instance in instances:
+        if ip_given:
+            if instance.public_ips[0] == instance_id_ip:
+                return instance
+        else:
+            if instance.name == instance_id_ip:
                 return instance
     return None
 
-def _wait_for_instance_to_terminate(connection, instance_ip_address):
-    instance = _get_this_instance(connection, instance_ip_address)
-    while instance.state == running:
-        print('Current status of Instance %s: %s'
-                % (instance_ip_address, instance.state))
-        
+
+def _wait_for_instance_to_start_running(settings, connection, instance):
+    instance_id = instance.name
+    while True:
+        instance = _get_this_instance(connection, instance_id)
+        instance_state = instance.state
+        instance_ip_list = instance.public_ips
+        print('Current status of Instance %s: %s, IP: %s'
+                % (instance_id, NODE_STATE[instance_state], instance_ip_list))
+        if instance_state == NodeState.RUNNING and len(instance_ip_list)>0:
+            return instance_ip_list[0]
+        time.sleep(settings.CLOUD_SLEEP_INTERVAL)
+
+
+def _wait_for_instance_to_terminate(settings, connection, ip_address):
+    instance = _get_this_instance(connection, ip_address, ip_given=True)
+    instance_id = instance.name
+    
+    while _is_instance_running(connection, ip_address):
+        print('Current status of Instance %s: %s, IP: [%s]'
+                % (instance_id, NODE_STATE[NodeState.RUNNING], ip_address))
         time.sleep(settings.CLOUD_SLEEP_INTERVAL)
     
+    print('Current status of Instance %s: %s, IP: [%s]'
+                % (instance_id, NODE_STATE[NodeState.TERMINATED], ip_address))
+
+
+def confirm_teardown(settings):
+    teardown_confirmation = None
+    while not teardown_confirmation:
+        teardown_confirmation = raw_input(
+                                "Are you sure you want to delete (yes/no)? ")
+        if teardown_confirmation != 'yes' and teardown_confirmation != 'no':
+            teardown_confirmation = None
+            
+    if teardown_confirmation == 'yes':
+        return True
+    else:
+        return False 
+
+    
+def _is_instance_running(connection, ip_address):
+    """
+        Checks whether an instance with @instance_id
+        is running or not
+    """
+    instance_running = False
+    all_instances = connection.list_nodes()
+    for instance in all_instances:
+        if instance.public_ips[0] == ip_address and instance.state == NodeState.RUNNING:
+            return True
+    return False
+       
 
