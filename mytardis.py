2414aa5ebef20c6bb75f6df6283a2569db8d38b5
iiman <iman.yusuf@rmit.edu.au>
2012-10-01 11:54:33 +1000

    Corret path to 'settings'  given in mytardis.py

--- mytardis.py
+++ mytardis.py
@@ -18,7 +18,7 @@ def start():
     '''
     import ConfigParser
     config = ConfigParser.RawConfigParser()
-    config_file = os.path.expanduser("~/.cloudenabling/config.sys")
+    config_file = os.path.expanduser("settings")
     if os.path.exists(config_file):
         config.read(config_file)
     else:
